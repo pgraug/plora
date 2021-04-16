e2e8002c9e212f45b527f47fb6dac383f10f1a16
Peter <petergnissen@gmail.com>
2021-04-16 17:51:22 +0200

    Added placeholder pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function PloraApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default PloraApp
