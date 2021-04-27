82a93db1992ec5c1e6baf1cce475de4059df3169
Peter <petergnissen@gmail.com>
2021-04-27 19:31:08 +0200

    Fixed Firebase Auth

--- utils/db/index.js
+++ utils/db/index.js
@@ -3,7 +3,18 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
+      credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "plora-xyz",
+        "private_key_id": process.env.PLORA_PRIVATE_KEY_ID,
+        "private_key": process.env.PLORA_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "client_email": "firebase-adminsdk-2a8zp@plora-xyz.iam.gserviceaccount.com",
+        "client_id": "104583202082695410289",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2a8zp%40plora-xyz.iam.gserviceaccount.com"
+      }),
       storageBucket: "plora-xyz.appspot.com"
     });
   } catch (error) {
